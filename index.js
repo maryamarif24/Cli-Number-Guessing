e4adbe9642d6cf050f7d143d522a33527b5e0cc7
Maryam Arif <160311643+maryamarif24@users.noreply.github.com>
2024-04-25 11:47:42 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "UserGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1-6:",
+    }
+]);
+if (answer.UserGuessedNumber === randomNumber) {
+    console.log("Congratulations! You guessed the right number.");
+}
+else {
+    console.log("You guessed the wrong number.");
+}
